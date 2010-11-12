3510eda491d55e42923f98d21956163dc8a5313e
John Brooks <john.brooks@bluecherry.net>
2010-11-12 15:24:34 -0700

    Enable image format autodetection in ImageDecodeTask
    This is less efficient than forcing it to use JPEG, but seems
    to work around a Qt bug present with Ubuntu 10.04. Fixes #473

--- src/utils/ImageDecodeTask.cpp
+++ src/utils/ImageDecodeTask.cpp
@@ -14,14 +14,23 @@ void ImageDecodeTask::runTask()
         return;
 
     QBuffer buffer(&m_data);
+    if (!buffer.open(QIODevice::ReadOnly))
+    {
+        qDebug() << "Image decoding error:" << buffer.errorString();
+        return;
+    }
+
     QImageReader reader(&buffer, "jpeg");
-    reader.setAutoDetectImageFormat(false);
+    /* This would be more efficient, but causes the decoding to fail with
+     * Qt 4.6.2 on Ubuntu 10.04. Disabled for now as a result. Issue #473 */
+    //reader.setAutoDetectImageFormat(false);
 
     if (!reader.read(&m_result))
     {
         qDebug() << "Image decoding error:" << reader.errorString();
         return;
     }
+    buffer.close();
     m_data.clear();
 
     m_scaleResults.resize(m_scaleSizes.size());
