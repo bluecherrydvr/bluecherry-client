df70a1a3819d798c907e2124e67fa7d5b40d8c27
John Brooks <john.brooks@bluecherry.net>
2011-07-01 18:24:27 +0100

    Bump version to 2.0.0-beta6

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     a.setOrganizationDomain(QLatin1String("bluecherrydvr.com"));
     a.setApplicationName(QLatin1String("bluecherry"));
 
-    a.setApplicationVersion(QLatin1String("2.0.0-beta5"));
+    a.setApplicationVersion(QLatin1String("2.0.0-beta6"));
 
 #ifdef Q_OS_WIN
     QSettings::setDefaultFormat(QSettings::IniFormat);
