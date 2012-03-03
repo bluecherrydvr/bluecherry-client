6bda7bdce2d07c20d1f93f494b974efa9c9d2617
John Brooks <john.brooks@bluecherry.net>
2012-03-03 16:22:10 -0700

    Correct RTSP URLs to match the server live-rtp branch

--- src/core/DVRCamera.cpp
+++ src/core/DVRCamera.cpp
@@ -89,7 +89,7 @@ bool DVRCamera::parseXML(QXmlStreamReader &xml)
     QUrl url;
     url.setScheme(QLatin1String("rtsp"));
     url.setHost(server()->api->serverUrl().host());
-    url.setPath(QString::fromLatin1("live_") + QString::number(d->uniqueID));
+    url.setPath(QString::fromLatin1("live/") + QString::number(d->uniqueID));
     d->streamUrl = url.toString().toLatin1();
     d->isLoaded = true;
 
