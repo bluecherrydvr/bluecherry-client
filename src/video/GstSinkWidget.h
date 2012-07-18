2d0d16602186c596844744319c7733983a4fd6a9
John Brooks <john.brooks@bluecherry.net>
2012-07-18 17:21:33 -0400

    Refactor GstSinkWidget to support non-GL viewports
    For systems without proper OpenGL support, it wasn't possible to
    play video within the client, due to a hard dependency on GL caused
    by inheriting directly from QGLWidget for the GstSinkWidget.
    
    This refactor merges VideoContainer (formerly the parent of a
    GstSinkWidget, due to dynamic creation and other needs), and
    renders actual video on a viewport child instead. As a result,
    GstSinkWidget now paints the frame and other logic, and handles
    the gstreamer integration, and the actual painting surface can be
    toggled.
    
    Integrated with the live view setting to allow manual enable/disable
    of OpenGL. Usual disclaimer with gstreamer stuff applies: this will
    all be gone when gstreamer goes.
    
    Bug #1085

--- src/video/GstSinkWidget.h
+++ src/video/GstSinkWidget.h
@@ -1,14 +1,14 @@
 #ifndef GSTSINKWIDGET_H
 #define GSTSINKWIDGET_H
 
-#include <QGLWidget>
+#include <QFrame>
 #include <QMutex>
 #include <gst/gst.h> // needed for GstFlowReturn
 
 typedef struct _GstAppSink GstAppSink;
 typedef void* gpointer;
 
-class GstSinkWidget : public QGLWidget
+class GstSinkWidget : public QFrame
 {
     Q_OBJECT
 
@@ -16,27 +16,40 @@ public:
     explicit GstSinkWidget(QWidget *parent = 0);
     ~GstSinkWidget();
 
+    GstElement *createElement();
+    void destroyElement();
     GstElement *gstElement() const { return GST_ELEMENT(m_element); }
     QImage currentFrame();
 
     virtual QSize sizeHint() const;
 
+    void setViewport(QWidget *viewport);
+
 public slots:
+    void setFullScreen(bool on);
+    void toggleFullScreen() { setFullScreen(!isFullScreen()); }
+
     void setOverlayMessage(const QString &message);
     void clearOverlayMessage() { setOverlayMessage(QString()); }
 
 private slots:
     void setBufferStatus(int percent);
+    void settingsChanged();
 
 protected:
-    virtual void paintEvent(QPaintEvent *ev);
+    virtual void resizeEvent(QResizeEvent *ev);
+    virtual void mouseDoubleClickEvent(QMouseEvent *ev);
+    virtual void keyPressEvent(QKeyEvent *ev);
+    virtual bool eventFilter(QObject *, QEvent *);
 
 private:
+    QWidget *m_viewport;
     GstAppSink *m_element;
     GstBuffer *m_framePtr;
     QMutex m_frameLock;
     QString m_overlayMsg;
     int m_frameWidth, m_frameHeight;
+    int m_normalFrameStyle;
 
     static void wrapEos(GstAppSink *sink, gpointer user_data);
     static GstFlowReturn wrapNewBuffer(GstAppSink *sink, gpointer user_data);
