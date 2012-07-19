bluecherrydvr/bluecherry-client

commit 2d0d16602186c596844744319c7733983a4fd6a9

Refactor GstSinkWidget to support non-GL viewports
For systems without proper OpenGL support, it wasn't possible to
play video within the client, due to a hard dependency on GL caused
by inheriting directly from QGLWidget for the GstSinkWidget.

This refactor merges VideoContainer (formerly the parent of a
GstSinkWidget, due to dynamic creation and other needs), and
renders actual video on a viewport child instead. As a result,
GstSinkWidget now paints the frame and other logic, and handles
the gstreamer integration, and the actual painting surface can be
toggled.

Integrated with the live view setting to allow manual enable/disable
of OpenGL. Usual disclaimer with gstreamer stuff applies: this will
all be gone when gstreamer goes.

Bug #1085

src/video/GstSinkWidget.cpp
@@ -1,67 +1,77 @@
 #include "GstSinkWidget.h"
+#include "core/BluecherryApp.h"
 #include <QPaintEvent>
 #include <QPainter>
 #include <QDebug>
 #include <QTimer>
 #include <QThread>
 #include <QApplication>
+#include <QGLWidget>
+#include <QSettings>
 #include <gst/gst.h>
 #include <gst/app/gstappsink.h>
 
 GstSinkWidget::GstSinkWidget(QWidget *parent)
-    : QGLWidget(parent), m_framePtr(0), m_frameWidth(-1), m_frameHeight(-1)
+    : QFrame(parent), m_viewport(0), m_element(0), m_framePtr(0), m_frameWidth(-1),
+      m_frameHeight(-1), m_normalFrameStyle(0)
 {
     setAutoFillBackground(false);
     setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    setAttribute(Qt::WA_OpaquePaintEvent);
 
-    m_element = GST_APP_SINK(gst_element_factory_make("appsink", "sinkwidget"));
-    if (!m_element)
+    setMinimumSize(320, 240);
+    setFrameStyle(QFrame::Sunken | QFrame::Panel);
+    QPalette p = palette();
+    p.setColor(QPalette::Window, Qt::black);
+    p.setColor(QPalette::WindowText, Qt::white);
+    setPalette(p);
+
+    QSettings settings;
+    if (!settings.value(QLatin1String("ui/liveview/disableHardwareAcceleration"), false).toBool())
+        setViewport(new QGLWidget);
+    else
     {
-        qWarning() << "GstSinkWidget: Creating appsink element failed";
-        return;
+        qDebug("Hardware-accelerated video output is DISABLED");
+        setViewport(new QWidget);
     }
 
-    g_object_ref(m_element);
-
-    GstCaps *caps = gst_caps_new_simple("video/x-raw-rgb",
-                                        "red_mask", G_TYPE_INT, 0xff00,
-                                        "blue_mask", G_TYPE_INT, 0xff000000,
-                                        "green_mask", G_TYPE_INT, 0xff0000,
-                                        NULL);
-    gst_app_sink_set_caps(m_element, caps);
-    gst_caps_unref(caps);
-
-    GstAppSinkCallbacks callbacks;
-    memset(&callbacks, 0, sizeof(callbacks));
-    callbacks.eos = &GstSinkWidget::wrapEos;
-    callbacks.new_buffer = &GstSinkWidget::wrapNewBuffer;
-    callbacks.new_preroll = &GstSinkWidget::wrapNewPreroll;
-    gst_app_sink_set_callbacks(m_element, &callbacks, this, NULL);
+    connect(bcApp, SIGNAL(settingsChanged()), SLOT(settingsChanged()));
 }
 
 GstSinkWidget::~GstSinkWidget()
 {
-    qDebug("gstreamer: Destroying sink widget");
+    destroyElement();
+}
 
-    /* Changing to NULL should always be synchronous; assertation should verify this. */
-    GstStateChangeReturn re = gst_element_set_state(GST_ELEMENT(m_element), GST_STATE_NULL);
-    Q_UNUSED(re);
-    Q_ASSERT(re == GST_STATE_CHANGE_SUCCESS);
-
-    /* At this point, it should not be possible to receive any more signals from the element,
-     * and the pipeline (if it still exists) is broken. */
-    g_object_unref(m_element);
-    m_element = 0;
-
-    m_frameLock.lock();
-    if (m_framePtr)
+void GstSinkWidget::setViewport(QWidget *w)
+{
+    if (m_viewport)
     {
-        /* Corresponding to the ref done in updateFrame prior to setting the value */
-        gst_buffer_unref(m_framePtr);
-        m_framePtr = 0;
+        m_viewport->removeEventFilter(this);
+        m_viewport->deleteLater();
     }
-    m_frameLock.unlock();
+
+    m_viewport = w;
+    m_viewport->setParent(this);
+    m_viewport->setGeometry(contentsRect());
+    m_viewport->setAutoFillBackground(false);
+    m_viewport->setAttribute(Qt::WA_OpaquePaintEvent);
+    m_viewport->installEventFilter(this);
+    m_viewport->show();
+}
+
+void GstSinkWidget::settingsChanged()
+{
+    QSettings settings;
+    bool hwaccel = !settings.value(QLatin1String("ui/liveview/disableHardwareAcceleration"), false).toBool();
+    bool old = m_viewport->inherits("QGLWidget");
+    if (hwaccel != old)
+    {
+        qDebug("%s hardware acceleration for video", hwaccel ? "Enabled" : "Disabled");
+        if (hwaccel)
+            setViewport(new QGLWidget);
+        else
+            setViewport(new QWidget);
+    }
 }
 
 QSize GstSinkWidget::sizeHint() const
@@ -83,8 +93,117 @@ void GstSinkWidget::setBufferStatus(int percent)
     Q_ASSERT(QThread::currentThread() == qApp->thread());
     if (percent == 100)
         clearOverlayMessage();
-    //else
-      //  setOverlayMessage(tr("Buffering\n%1%").arg(percent));
+}
+
+void GstSinkWidget::setFullScreen(bool on)
+{
+    if (on)
+    {
+        setWindowFlags(windowFlags() | Qt::Window);
+        m_normalFrameStyle = frameStyle();
+        setFrameStyle(QFrame::NoFrame);
+        showFullScreen();
+    }
+    else
+    {
+        setWindowFlags(windowFlags() & ~Qt::Window);
+        setFrameStyle(m_normalFrameStyle);
+        showNormal();
+    }
+
+    QSettings settings;
+    if (settings.value(QLatin1String("ui/disableScreensaver/onFullscreen")).toBool())
+        bcApp->setScreensaverInhibited(on);
+}
+
+void GstSinkWidget::resizeEvent(QResizeEvent *ev)
+{
+    QFrame::resizeEvent(ev);
+    m_viewport->setGeometry(contentsRect());
+}
+
+void GstSinkWidget::mouseDoubleClickEvent(QMouseEvent *ev)
+{
+    ev->accept();
+    toggleFullScreen();
+}
+
+void GstSinkWidget::keyPressEvent(QKeyEvent *ev)
+{
+    if (ev->modifiers() != 0)
+        return;
+
+    switch (ev->key())
+    {
+    case Qt::Key_Escape:
+        setFullScreen(false);
+        break;
+    default:
+        return;
+    }
+
+    ev->accept();
+}
+
+GstElement *GstSinkWidget::createElement()
+{
+    Q_ASSERT(!m_element);
+    if (m_element)
+        return GST_ELEMENT(m_element);
+
+    m_element = GST_APP_SINK(gst_element_factory_make("appsink", "sinkwidget"));
+    if (!m_element)
+    {
+        qWarning() << "GstSinkWidget: Creating appsink element failed";
+        return NULL;
+    }
+
+    g_object_ref(m_element);
+
+    GstCaps *caps = gst_caps_new_simple("video/x-raw-rgb",
+                                        "red_mask", G_TYPE_INT, 0xff00,
+                                        "blue_mask", G_TYPE_INT, 0xff000000,
+                                        "green_mask", G_TYPE_INT, 0xff0000,
+                                        NULL);
+    gst_app_sink_set_caps(m_element, caps);
+    gst_caps_unref(caps);
+
+    GstAppSinkCallbacks callbacks;
+    memset(&callbacks, 0, sizeof(callbacks));
+    callbacks.eos = &GstSinkWidget::wrapEos;
+    callbacks.new_buffer = &GstSinkWidget::wrapNewBuffer;
+    callbacks.new_preroll = &GstSinkWidget::wrapNewPreroll;
+    gst_app_sink_set_callbacks(m_element, &callbacks, this, NULL);
+
+    return GST_ELEMENT(m_element);
+}
+
+void GstSinkWidget::destroyElement()
+{
+    qDebug("gstreamer: Destroying sink widget");
+
+    /* Changing to NULL should always be synchronous; assertation should verify this. */
+    GstStateChangeReturn re = gst_element_set_state(GST_ELEMENT(m_element), GST_STATE_NULL);
+    Q_UNUSED(re);
+    Q_ASSERT(re == GST_STATE_CHANGE_SUCCESS);
+
+    /* At this point, it should not be possible to receive any more signals from the element,
+     * and the pipeline (if it still exists) is broken. */
+    g_object_unref(m_element);
+    m_element = 0;
+
+    m_frameLock.lock();
+    if (m_framePtr)
+    {
+        /* Corresponding to the ref done in updateFrame prior to setting the value */
+        gst_buffer_unref(m_framePtr);
+        m_framePtr = 0;
+    }
+    m_frameLock.unlock();
+
+    m_frameWidth = -1;
+    m_frameHeight = -1;
+    clearOverlayMessage();
 }
 
 QImage GstSinkWidget::currentFrame()
@@ -108,16 +227,16 @@ QImage GstSinkWidget::currentFrame()
     return re;
 }
 
-/* It is technically possible to draw into QGLWidget from another thread, if things are changed
- * to protect the context. This would provide a big benefit in terms of latency and CPU usage
- * here. */
-
-void GstSinkWidget::paintEvent(QPaintEvent *ev)
+bool GstSinkWidget::eventFilter(QObject *obj, QEvent *ev)
 {
-    Q_UNUSED(ev);
+    Q_ASSERT(obj == m_viewport);
 
-    QPainter p(this);
-    p.setRenderHint(QPainter::SmoothPixmapTransform);
+    if (ev->type() != QEvent::Paint)
+        return QFrame::eventFilter(obj, ev);
+
+    QPainter p(m_viewport);
+    if (p.device()->paintEngine()->type() == QPaintEngine::OpenGL2)
+        p.setRenderHint(QPainter::SmoothPixmapTransform);
     p.setBackground(QColor(Qt::black));
 
     m_frameLock.lock();
@@ -129,7 +248,7 @@ void GstSinkWidget::paintEvent(QPaintEvent *ev)
     if (!buffer)
     {
         p.fillRect(rect(), Qt::black);
-        return;
+        return true;
     }
 
     QImage frame = QImage(GST_BUFFER_DATA(buffer), m_frameWidth, m_frameHeight, QImage::Format_RGB32);
@@ -163,6 +282,8 @@ void GstSinkWidget::paintEvent(QPaintEvent *ev)
         p.setRenderHint(QPainter::TextAntialiasing);
         p.drawText(textRect, Qt::AlignCenter, m_overlayMsg);
     }
+
+    return true;
 }
 
 void GstSinkWidget::endOfStream()

client.pro
@@ -201,7 +201,6 @@ HEADERS  += src/ui/MainWindow.h \
     src/ui/EventVideoPlayer.h \
     src/video/VideoPlayerBackend_gst.h \
     src/video/VideoHttpBuffer.h \
-    src/video/VideoContainer.h \
     src/ui/EventVideoDownload.h \
     src/ui/ServerConfigWindow.h \
     src/ui/AboutDialog.h \

src/ui/EventVideoPlayer.cpp
@@ -1,6 +1,5 @@
 #include "EventVideoPlayer.h"
 #include "EventVideoDownload.h"
-#include "video/VideoContainer.h"
 #include "video/GstSinkWidget.h"
 #include "video/VideoHttpBuffer.h"
 #include "core/BluecherryApp.h"
@@ -26,6 +25,7 @@
 #include <QDesktopServices>
 #include <QNetworkAccessManager>
 #include <QNetworkCookieJar>
+#include <QMouseEvent>
 #include <math.h>
 
 /* Hack to disable the "page step" behavior for left click on non-Mac. For seeking, especially in
@@ -67,11 +67,11 @@ EventVideoPlayer::EventVideoPlayer(QWidget *parent)
     QBoxLayout *layout = new QVBoxLayout(this);
     layout->setMargin(0);
 
-    m_videoContainer = new VideoContainer;
-    m_videoContainer->setFrameStyle(QFrame::NoFrame);
-    m_videoContainer->setContextMenuPolicy(Qt::CustomContextMenu);
-    connect(m_videoContainer, SIGNAL(customContextMenuRequested(QPoint)), SLOT(videoContextMenu(QPoint)));
-    layout->addWidget(m_videoContainer, 1);
+    m_videoWidget = new GstSinkWidget;
+    m_videoWidget->setFrameStyle(QFrame::NoFrame);
+    m_videoWidget->setContextMenuPolicy(Qt::CustomContextMenu);
+    connect(m_videoWidget, SIGNAL(customContextMenuRequested(QPoint)), SLOT(videoContextMenu(QPoint)));
+    layout->addWidget(m_videoWidget, 1);
 
     QBoxLayout *controlsLayout = new QVBoxLayout;
     controlsLayout->setContentsMargins(style()->pixelMetric(QStyle::PM_LayoutLeftMargin), 0,
@@ -137,19 +137,19 @@ EventVideoPlayer::EventVideoPlayer(QWidget *parent)
     btnLayout->addWidget(m_saveBtn);
     connect(m_saveBtn, SIGNAL(clicked()), SLOT(saveVideo()));
 
-    QShortcut *sc = new QShortcut(QKeySequence(Qt::Key_Space), m_videoContainer);
+    QShortcut *sc = new QShortcut(QKeySequence(Qt::Key_Space), m_videoWidget);
     connect(sc, SIGNAL(activated()), SLOT(playPause()));
 
-    sc = new QShortcut(QKeySequence(Qt::Key_F), m_videoContainer);
-    connect(sc, SIGNAL(activated()), m_videoContainer, SLOT(toggleFullScreen()));
+    sc = new QShortcut(QKeySequence(Qt::Key_F), m_videoWidget);
+    connect(sc, SIGNAL(activated()), m_videoWidget, SLOT(toggleFullScreen()));
 
-    sc = new QShortcut(QKeySequence(Qt::Key_R), m_videoContainer);
+    sc = new QShortcut(QKeySequence(Qt::Key_R), m_videoWidget);
     connect(sc, SIGNAL(activated()), SLOT(restart()));
 
-    sc = new QShortcut(QKeySequence::Save, m_videoContainer);
+    sc = new QShortcut(QKeySequence::Save, m_videoWidget);
     connect(sc, SIGNAL(activated()), SLOT(saveVideo()));
 
-    sc = new QShortcut(QKeySequence(Qt::Key_F5), m_videoContainer);
+    sc = new QShortcut(QKeySequence(Qt::Key_F5), m_videoWidget);
     connect(sc, SIGNAL(activated()), SLOT(saveSnapshot()));
 
     setControlsEnabled(false);
@@ -200,9 +200,9 @@ void EventVideoPlayer::setVideo(const QUrl &url, EventData *event)
     connect(m_video, SIGNAL(endOfStream()), SLOT(durationChanged()));
     connect(m_video, SIGNAL(playbackSpeedChanged(double)), SLOT(playbackSpeedChanged(double)));
 
-    m_videoWidget = new GstSinkWidget;
-    m_videoContainer->setInnerWidget(m_videoWidget);
-    m_video->setSink(m_videoWidget->gstElement());
+    GstElement *sink = m_videoWidget->createElement();
+    Q_ASSERT(sink);
+    m_video->setSink(sink);
 
     connect(m_video, SIGNAL(bufferingStatus(int)), m_videoWidget, SLOT(setBufferStatus(int)));
     connect(m_video->videoBuffer(), SIGNAL(bufferingStopped()), SLOT(bufferingStopped()), Qt::QueuedConnection);
@@ -248,7 +248,7 @@ void EventVideoPlayer::clearVideo()
     m_statusText->clear();
     m_rateText->clear();
     m_uiTimer.stop();
-    m_videoContainer->setInnerWidget(0);
+    m_videoWidget->destroyElement();
     setControlsEnabled(false);
 }
 
@@ -580,9 +580,9 @@ void EventVideoPlayer::videoContextMenu(const QPoint &rpos)
     menu.addSeparator();
 
     if (m_videoWidget->isFullScreen())
-        menu.addAction(tr("Exit &full screen"), m_videoContainer, SLOT(toggleFullScreen()));
+        menu.addAction(tr("Exit &full screen"), m_videoWidget, SLOT(toggleFullScreen()));
     else
-        menu.addAction(tr("&Full screen"), m_videoContainer, SLOT(toggleFullScreen()));
+        menu.addAction(tr("&Full screen"), m_videoWidget, SLOT(toggleFullScreen()));
 
     menu.addSeparator();
 

src/ui/EventVideoPlayer.h
@@ -14,7 +14,6 @@ class QPushButton;
 class QSlider;
 class QLabel;
 class GstSinkWidget;
-class VideoContainer;
 class QThread;
 struct EventData;
 
@@ -58,7 +57,6 @@ private:
     EventData *m_event;
     QThread *m_videoThread;
     VideoPlayerBackend *m_video;
-    VideoContainer *m_videoContainer;
     GstSinkWidget *m_videoWidget;
     QToolButton *m_playBtn, *m_restartBtn, *m_fastBtn, *m_slowBtn;
     QPushButton *m_saveBtn;

src/video/GstSinkWidget.h
@@ -1,14 +1,14 @@
 #ifndef GSTSINKWIDGET_H
 #define GSTSINKWIDGET_H
 
-#include <QGLWidget>
+#include <QFrame>
 #include <QMutex>
 #include <gst/gst.h> // needed for GstFlowReturn
 
 typedef struct _GstAppSink GstAppSink;
 typedef void* gpointer;
 
-class GstSinkWidget : public QGLWidget
+class GstSinkWidget : public QFrame
 {
     Q_OBJECT
 
@@ -16,27 +16,40 @@ public:
     explicit GstSinkWidget(QWidget *parent = 0);
     ~GstSinkWidget();
 
+    GstElement *createElement();
+    void destroyElement();
     GstElement *gstElement() const { return GST_ELEMENT(m_element); }
     QImage currentFrame();
 
     virtual QSize sizeHint() const;
 
+    void setViewport(QWidget *viewport);
+
 public slots:
+    void setFullScreen(bool on);
+    void toggleFullScreen() { setFullScreen(!isFullScreen()); }
+
     void setOverlayMessage(const QString &message);
     void clearOverlayMessage() { setOverlayMessage(QString()); }
 
 private slots:
     void setBufferStatus(int percent);
+    void settingsChanged();
 
 protected:
-    virtual void paintEvent(QPaintEvent *ev);
+    virtual void resizeEvent(QResizeEvent *ev);
+    virtual void mouseDoubleClickEvent(QMouseEvent *ev);
+    virtual void keyPressEvent(QKeyEvent *ev);
+    virtual bool eventFilter(QObject *, QEvent *);
 
 private:
+    QWidget *m_viewport;
     GstAppSink *m_element;
     GstBuffer *m_framePtr;
     QMutex m_frameLock;
     QString m_overlayMsg;
     int m_frameWidth, m_frameHeight;
+    int m_normalFrameStyle;
 
     static void wrapEos(GstAppSink *sink, gpointer user_data);
     static GstFlowReturn wrapNewBuffer(GstAppSink *sink, gpointer user_data);

src/video/VideoContainer.h
@@ -1,113 +1,0 @@
-#ifndef VIDEOCONTAINER_H
-#define VIDEOCONTAINER_H
-
-#include "core/BluecherryApp.h"
-#include <QFrame>
-#include <QMouseEvent>
-#include <QSettings>
-#include <QDebug>
-
-class VideoContainer : public QFrame
-{
-    Q_OBJECT
-
-public:
-    VideoContainer(QWidget *inner = 0, QWidget *parent = 0)
-        : QFrame(parent), innerWidget(0)
-    {
-        setMinimumSize(320, 240);
-        setFrameStyle(QFrame::Sunken | QFrame::Panel);
-        setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-
-        QPalette p = palette();
-        p.setColor(QPalette::Window, Qt::black);
-        p.setColor(QPalette::WindowText, Qt::white);
-        setPalette(p);
-
-        if (inner)
-            setInnerWidget(inner);
-    }
-
-    void setInnerWidget(QWidget *inner)
-    {
-        if (innerWidget)
-        {
-            innerWidget->hide();
-            innerWidget->deleteLater();
-        }
-
-        innerWidget = inner;
-
-        if (innerWidget)
-        {
-            innerWidget->setParent(this);
-            innerWidget->setGeometry(contentsRect());
-            innerWidget->show();
-        }
-    }
-
-public slots:
-    void setFullScreen(bool on)
-    {
-        if (on)
-        {
-            setWindowFlags(windowFlags() | Qt::Window);
-            m_normalFrameStyle = frameStyle();
-            setFrameStyle(QFrame::NoFrame);
-            showFullScreen();
-        }
-        else
-        {
-            setWindowFlags(windowFlags() & ~Qt::Window);
-            setFrameStyle(m_normalFrameStyle);
-            showNormal();
-        }
-
-        QSettings settings;
-        if (settings.value(QLatin1String("ui/disableScreensaver/onFullscreen")).toBool())
-            bcApp->setScreensaverInhibited(on);
-    }
-
-    void toggleFullScreen()
-    {
-        setFullScreen(!isFullScreen());
-    }
-
-protected:
-    QWidget *innerWidget;
-    int m_normalFrameStyle;
-
-    virtual void resizeEvent(QResizeEvent *ev)
-    {
-        QFrame::resizeEvent(ev);
-        if (innerWidget)
-            innerWidget->setGeometry(contentsRect());
-    }
-
-    virtual void mouseDoubleClickEvent(QMouseEvent *ev)
-    {
-        ev->accept();
-        toggleFullScreen();
-    }
-
-    virtual void keyPressEvent(QKeyEvent *ev)
-    {
-        if (ev->modifiers() != 0)
-            return;
-
-        switch (ev->key())
-        {
-        case Qt::Key_Escape:
-            setFullScreen(false);
-            break;
-
-        default:
-            return;
-        }
-
-        ev->accept();
-    }
-};
-
-
-#endif // VIDEOCONTAINER_H

commit a00d0d5c7a7fc81ac5a39cdbba6d831069687b9e

Fix occasional crash when closing video windows
Regression caused by 2d0d1660. This would attempt to destroy the
element in some cases when there was no element allocated.

src/video/GstSinkWidget.cpp
@@ -180,17 +180,20 @@ GstElement *GstSinkWidget::createElement()
 
 void GstSinkWidget::destroyElement()
 {
-    qDebug("gstreamer: Destroying sink widget");
+    if (m_element)
+    {
+        qDebug("gstreamer: Destroying sink widget");
 
-    /* Changing to NULL should always be synchronous; assertation should verify this. */
-    GstStateChangeReturn re = gst_element_set_state(GST_ELEMENT(m_element), GST_STATE_NULL);
-    Q_UNUSED(re);
-    Q_ASSERT(re == GST_STATE_CHANGE_SUCCESS);
+        /* Changing to NULL should always be synchronous; assertation should verify this. */
+        GstStateChangeReturn re = gst_element_set_state(GST_ELEMENT(m_element), GST_STATE_NULL);
+        Q_UNUSED(re);
+        Q_ASSERT(re == GST_STATE_CHANGE_SUCCESS);
 
-    /* At this point, it should not be possible to receive any more signals from the element,
-     * and the pipeline (if it still exists) is broken. */
-    g_object_unref(m_element);
-    m_element = 0;
+        /* At this point, it should not be possible to receive any more signals from the element,
+         * and the pipeline (if it still exists) is broken. */
+        g_object_unref(m_element);
+        m_element = 0;
+    }
 
     m_frameLock.lock();
     if (m_framePtr)