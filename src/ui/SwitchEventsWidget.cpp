f76872a126db8d05d021d2e89d21f94306dc4ec8
Rafał Malinowski <rafal.malinowski@corp.bluecherry.net>
2013-01-19 12:11:19 +0100

    Feature #1032: Added SwitchEventsWidget class
    Simple widget with previous/next button to switch between
    events.
    
    Signed-off-by: Rafał Malinowski <rafal.malinowski@corp.bluecherry.net>

---
+++ src/ui/SwitchEventsWidget.cpp
@@ -0,0 +1,60 @@
+#include "SwitchEventsWidget.h"
+#include "events/EventsCursor.h"
+#include <QDebug>
+#include <QHBoxLayout>
+#include <QPushButton>
+
+SwitchEventsWidget::SwitchEventsWidget(QWidget *parent)
+    : QWidget(parent)
+{
+    QHBoxLayout *layout = new QHBoxLayout(this);
+    // layout->setMargin(0);
+
+    m_previousButton = new QPushButton(tr("Previous"));
+    m_nextButton = new QPushButton(tr("Next"));
+
+    layout->addWidget(m_previousButton);
+    layout->addWidget(m_nextButton);
+
+    updateButtonsState();
+}
+
+SwitchEventsWidget::~SwitchEventsWidget()
+{
+}
+
+void SwitchEventsWidget::setEventsCursor(const QSharedPointer<EventsCursor> &eventsCursor)
+{
+    EventsCursor *oldCursor = m_eventsCursor.data();
+    if (m_eventsCursor.data())
+    {
+        disconnect(m_previousButton, 0, oldCursor, 0);
+        disconnect(m_nextButton, 0, oldCursor, 0);
+        disconnect(oldCursor, 0, this, 0);
+    }
+
+    m_eventsCursor = eventsCursor;
+
+    if (m_eventsCursor)
+    {
+        connect(m_previousButton, SIGNAL(clicked()), m_eventsCursor.data(), SLOT(moveToPrevious()));
+        connect(m_nextButton, SIGNAL(clicked()), m_eventsCursor.data(), SLOT(moveToNext()));
+        connect(eventsCursor.data(), SIGNAL(eventSwitched(EventData*)), this, SLOT(updateButtonsState()));
+    }
+
+    updateButtonsState();
+}
+
+void SwitchEventsWidget::updateButtonsState()
+{
+    if (!m_eventsCursor)
+    {
+        m_previousButton->setEnabled(false);
+        m_nextButton->setEnabled(false);
+    }
+    else
+    {
+        m_previousButton->setEnabled(m_eventsCursor.data()->hasPrevious());
+        m_nextButton->setEnabled(m_eventsCursor.data()->hasNext());
+    }
+}
