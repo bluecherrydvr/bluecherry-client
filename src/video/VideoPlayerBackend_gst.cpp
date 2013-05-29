71c2ad25550a0752d831e325b3c0e784ad239d60
Rafał Malinowski <rafal.malinowski@corp.bluecherry.net>
2013-05-29 23:36:28 +0200

    Some more fixes for new gstreamer

--- src/video/VideoPlayerBackend_gst.cpp
+++ src/video/VideoPlayerBackend_gst.cpp
@@ -105,7 +105,7 @@ bool VideoPlayerBackend::initGStreamer(QString *errorMessage)
 #if defined(Q_OS_MAC)
     QString pluginPath = QApplication::applicationDirPath() + QLatin1String("/../PlugIns/gstreamer/");
 #else
-    QString pluginPath = QDir::toNativeSeparators(QApplication::applicationDirPath() + QLatin1String("/"));
+    QString pluginPath = QDir::toNativeSeparators(QApplication::applicationDirPath() + QDir::separator());
 #endif
 
 #ifdef GSTREAMER_PLUGINS
@@ -113,7 +113,7 @@ bool VideoPlayerBackend::initGStreamer(QString *errorMessage)
     if (QDir::isAbsolutePath(ppx))
         pluginPath = ppx;
     else
-        pluginPath += QDir::separator() + ppx;
+        pluginPath += ppx;
 #endif
 
     if (!QFile::exists(pluginPath))
