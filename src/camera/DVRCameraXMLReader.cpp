f46ed31c4d231e0972b1f3958467e093a8927679
Rafał Malinowski <rafal.malinowski@corp.bluecherry.net>
2013-07-12 17:46:05 +0200

    Small cleanup
    Signed-off-by: Rafał Malinowski <rafal.malinowski@corp.bluecherry.net>

--- src/camera/DVRCameraXMLReader.cpp
+++ src/camera/DVRCameraXMLReader.cpp
@@ -15,8 +15,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "DVRCamera.h"
 #include "DVRCameraXMLReader.h"
+#include "camera/DVRCamera.h"
 #include "server/DVRServer.h"
 #include <QUrl>
 
