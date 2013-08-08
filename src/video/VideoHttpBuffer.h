483ae85bd9ffb305a12df093ae9f5b4b3111383e
Rafał Malinowski <rafal.malinowski@corp.bluecherry.net>
2013-08-08 17:04:55 +0200

    Move all GST dependencies out of VideoHttpBuffer
    Signed-off-by: Rafał Malinowski <rafal.malinowski@corp.bluecherry.net>

--- src/video/VideoHttpBuffer.h
+++ src/video/VideoHttpBuffer.h
@@ -22,21 +22,17 @@
 #include <QUrl>
 #include "MediaDownload.h"
 
-typedef struct _GstAppSrc GstAppSrc;
-typedef struct _GstElement GstElement;
 class QNetworkCookie;
 
 class VideoHttpBuffer : public QObject
 {
     Q_OBJECT
 
+    friend class GstVideoHttpBuffer;
+
 public:
     explicit VideoHttpBuffer(const QUrl &url, QObject *parent = 0);
     ~VideoHttpBuffer();
-
-    /* Create and prepare a source element; the element will be added to the pipeline,
-     * but not linked. */
-    GstElement *setupSrcElement(GstElement *pipeline);
 
     bool isBuffering() const { return media && !media->isFinished(); }
 
@@ -48,36 +44,24 @@ public:
 
     QUrl url() const { return m_url; }
 
-public slots:
-    void clearPlayback();
-
 signals:
     void streamError(const QString &message);
 
     /* Emitted when buffering starts, i.e. upon start() */
     void bufferingStarted();
-    /* Emitted when there is data ready in the buffer for the first time */
-    void bufferingReady();
     /* Emitted when buffering stops for any reason, including errors */
     void bufferingStopped();
     /* Emitted when buffering is finished, and the entire file is cached locally */
     void bufferingFinished();
+    void sizeChanged(unsigned size);
 
 private slots:
-    void fileSizeChanged(unsigned fileSize);
     void sendStreamError(const QString &message);
 
 private:
     QUrl m_url;
     MediaDownload *media;
-    GstAppSrc *m_element;
-    GstElement *m_pipeline;
 
-    static void needDataWrap(GstAppSrc *, unsigned, void*);
-    static int seekDataWrap(GstAppSrc *, quint64, void*);
-
-    void needData(int size);
-    bool seekData(qint64 offset);
 };
 
 inline int VideoHttpBuffer::bufferedPercent() const
