185b6b0ffbe376ecf89db5e680e4dcb28354328c
Anton <anton@corp.bluecherry.net>
2016-10-10 02:04:06 +0400

    updated build script

--- build_cygwin_x86_64-w64-mingw32.sh
+++ build_cygwin_x86_64-w64-mingw32.sh
@@ -2,10 +2,14 @@
 
 export PATH="/usr/lib/qt4/x86_64-w64-mingw32/bin:$PATH"
 
+rm build_w64 -rf
 mkdir build_w64
+cd build_w64
 
 cp ../x86_64-w64-mingw32.cmake ./
 
 cmake  -DCMAKE_TOOLCHAIN_FILE=x86_64-w64-mingw32.cmake  -DCMAKE_BUILD_TYPE=Release -DWIN32=true ..
+
+make
 
 
