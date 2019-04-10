8b34720a64ff8a93a72afb9fc21ca22b03e80b34
Curtis Hall <curt@bluecherry.net>
2019-04-09 22:51:17 -0500

    Update copyright year.

--- src/audio/AudioPlayer.cpp
+++ src/audio/AudioPlayer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2016 Bluecherry
+ * Copyright 2010-2019 Bluecherry
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
